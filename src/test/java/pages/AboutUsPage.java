dd57ef6154a504412291511aaa01c8670dcee9c2
benzeta <benazeta70@gmail.com>
2024-05-16 07:22:51 +0100

    commit 1

--- src/test/java/pages/AboutUsPage.java
+++ src/test/java/pages/AboutUsPage.java
@@ -2,15 +2,11 @@ package pages;
 
 import helper.GenericMethods;
 import helper.LoggerHelper;
-import helper.assertion.VerificationHelper;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import utilities.GlobalVars;
 
-import java.time.Duration;
 import java.util.List;
 
 public class AboutUsPage {
@@ -24,9 +20,6 @@ public class AboutUsPage {
     @FindBy(xpath = "//div[@class='score-style-box clearfix']//a[@class='score-button btn-clickable-area'][normalize-space()='Learn More']")
     private WebElement careersLink;
 
-    public List<WebElement> getAllAboutUsLinks() {
-        return allLinks;
-    }
 
     public CareersPage clickCareersLink() {
         if (careersLink.isDisplayed() && careersLink.isEnabled()) {
@@ -43,41 +36,14 @@ public class AboutUsPage {
                 .anyMatch(s -> s.getText().equalsIgnoreCase(linkText));
     }
 
-    public String getCareersLinkText() {
-        System.out.println(careersLink.getText());
-        log.info("Getting Careers Link Text");
-        return new VerificationHelper(driver).getText(careersLink);
-    }
-
-    public boolean assertAboutUsTextIsDisplayed() {
-        log.info("Asserting About Us Link");
-        return new VerificationHelper(driver).isDisplayed(aboutUsHeadingText);
-    }
-
     public String getAboutUsText() {
         log.info("Getting About Us Text");
         return aboutUsHeadingText.getText().trim();
     }
 
-    public boolean assertLinkByText(String linkText) {
-        log.info("Asserting linkText");
-        return allLinks.stream().parallel().anyMatch(link -> link.getText().equalsIgnoreCase(linkText));
-    }
-
-    public boolean assertAllLinksAreDisplayed() {
-        log.info("Asserting All Links");
-        return allLinks.stream().parallel().allMatch(link -> link.isDisplayed() && link.isEnabled());
-    }
-
     public long getAllLinksCount() {
         log.info("Asserting all links count");
         return allLinks.stream().parallel().filter(WebElement::isDisplayed).count();
-    }
-
-    public void clickAnElementMatchingTextInAboutUs(List<WebElement> elements, String linkText) {
-        new WebDriverWait(driver, Duration.ofSeconds(GlobalVars.explicitWait)).until((d) -> elements.size() > 6);
-        elements.stream().parallel().filter(item -> item.getText().contains(linkText)).findFirst().ifPresent(s -> s.click());
-        log.info("Matching link text is clicked");
     }
 
     public List<String> getAboutUsTitlesList() {
